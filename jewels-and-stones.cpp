b4173da9fb7cb2704e6f70bca28ec1aa76f0fd45
Prakhar Pipersania <pipersania.prakhar@gmail.com>
2020-10-01 00:46:59 +0530

    Leetcode Problem 771

---
+++ jewels-and-stones.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int numJewelsInStones(string J, string S) 
+    {
+        int i=0,j,k=0,sum=0;
+        while(J[i]!='\0')
+        {
+            k=j=0;
+            while(S[j]!='\0')
+            {
+                if(J[i]==S[j])
+                    k++;
+                j++;
+            }
+            sum+=k;
+            i++;
+        }
+        return sum;    
+    }
+};
